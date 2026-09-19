8eacfad46d30541a3ea4b04f227db45d49d83590
Rexa-46 <r.sh.shadfar@gmail.com>
2026-09-19 17:11:19 +0330

    Update RexaNativePlugin.java

--- plugins/rexa-native/android/src/main/java/ir/shadfar/rexa/RexaNativePlugin.java
+++ plugins/rexa-native/android/src/main/java/ir/shadfar/rexa/RexaNativePlugin.java
@@ -13,8 +13,8 @@ import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
 import android.telephony.SmsMessage;
 
-import android.support.annotation.NonNull;
-import android.support.v4.content.ContextCompat;
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 
 import com.getcapacitor.JSArray;
 import com.getcapacitor.JSObject;
